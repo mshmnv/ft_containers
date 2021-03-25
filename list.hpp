23cb8eccc3bd385d4bf8d27c7ee53b22a2c59982
Mariq Minaewa <lbagg@ox-r3.msk.21-school.ru>
2021-03-25 16:37:54 +0300

    start

---
+++ list.hpp
@@ -0,0 +1,52 @@
+#ifndef LIST_HPP
+#define LIST_HPP
+
+class List {
+
+private:
+
+public:
+	List();
+	~List();
+
+	List &operator=(List const &list);
+
+	// void assign( size_type count, const T& value );
+	// template< class InputIt >
+	// void assign( InputIt first, InputIt last );
+	// assign();
+
+
+	// reference front();
+	// const_reference front() const;
+
+	// reference back();
+	// const_reference back() const;
+
+
+	// ITERATORS
+	// begin
+	// end
+	// rbegin
+	// rend
+
+	// empty();
+	// size();
+	// max_size();
+
+
+	// clear();
+	// insert();
+	// erase();
+	// push_back();
+	// pop_back();
+	// push_front();
+	// pop_front();
+	// resize();
+	// swap();
+
+
+
+};
+
+#endif
